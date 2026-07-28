fdf77920d82fa56c967687624f940cb038d44ec5
hardik gupta <hg08072005@gmail.com>
2026-07-28 13:55:39 +0530

    Time: 48 ms (18.68%), Space: 42.7 MB (56.23%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,47 +5,24 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        n=self.findlength(head)
-        if n==1:
-            return True
-        reversepoint=n//2
-        if (n%2 == 1):
-            reversepoint = n//2 +1
-        head2=self.findloc(head,reversepoint)
-        prev=self.findloc(head,reversepoint-1)
-        self.reverse(head2,prev)
-        first= head
-        second=prev.next
-        while first is not None and second is not None:
-            if (first.val != second.val):
-                return False
-            first = first.next
-            second = second.next
-        return True
-
-    def findloc(self,head,pos):
-        count=0
-        curr=head
-        while (count<pos):
-            count+=1
-            curr=curr.next
-        return curr
-
-    def findlength(self,head):
-        length=0
-        curr=head
+        slow,fast=head,head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        
+        prev=None
+        curr=slow
         while curr:
-            length+=1
-            curr=curr.next
-        return length
-    def reverse(self,head,prev):
-        f=head
-        s=head.next
-        while f is not None and s is not None:
-            temp=s.next
-            s.next=f
-            f=s
-            s=temp
-        head.next=None
-        prev.next=f
+            nxt=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nxt
+        
+        first,second=head,prev
+        while second:
+            if first.val != second.val:
+                return False
+            first=first.next
+            second=second.next
+        return True
         
