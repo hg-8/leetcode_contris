b16b4d324bbf683105dd04a331c0333215aa4b9c
hardik gupta <hg08072005@gmail.com>
2026-08-19 16:29:42 +0530

    Time: 198 ms (39.9%), Space: 34.3 MB (96.96%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,24 +1,14 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        n=len(nums)
-        ans=[]
         de=Deque()
-        de.append(0)
-        for i in range(1,k):
-            while(len(de)>0 and nums[de[-1]]<nums[i]):
+        ans=[]
+        for i,val in enumerate(nums):
+            if de and de[0]<i-k+1:
+                de.popleft()
+            while de and nums[de[-1]]<val:
                 de.pop()
             de.append(i)
-        ans.append(nums[de[0]])
-        for j in range(k,n):
-        #why k? -> cause first k elements wouldnt have this j-k+1 startpt
-            startptwind=j-k+1
-            while(len(de)>0 and de[0]<startptwind):
-                de.popleft()
-            while(len(de)>0 and nums[de[-1]]<nums[j]):
-            #this is because we want to basically get the max number in that window so we have one by one comparing them with current j index and then appending j
-                de.pop()
-            de.append(j)
-            ans.append(nums[de[0]])
+            if i>=k-1:
+                ans.append(nums[de[0]])
         return ans
-        
         
